7b67b8afb370c17f124bdfe3a82ffcfbb3282a2a
Anthony <an538811@gmail.com>
2017-10-14 11:08:08 -0500

    media query working

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
-// var connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: 'diagnoser'
-// });
-var connection = mysql.createConnection(`mysql://root:madonna@localhost:3306/diagnoser`);
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'an538811',
+    database: 'burgers_db'
+});
+// var connection = mysql.createConnection(`mysql://root:madonna@localhost:3306/diagnoser`);
 
 connection.connect(function(err) {
     if (err) {
